c40da75a7354659c2741550d51b2cb606272a18a
dylanmryan-ucla <dylanmryan@g.ucla.edu>
2025-06-10 20:59:24 -0700

    trying only gravity comp

--- src/Project_InverseDynamicsControl.py
+++ src/Project_InverseDynamicsControl.py
@@ -175,12 +175,13 @@ class InverseDynamicsController:
             print(B_q @ y)
 
             #Nonlinear Components
-            n = (self.compute_coriolis_matrix(q_rad, qdot_rad_per_s) @ qdot_rad_per_s) - self.calc_gravity_compensation_torque(q_rad)
+            n = (self.compute_coriolis_matrix(q_rad, qdot_rad_per_s) @ qdot_rad_per_s) + self.calc_gravity_compensation_torque(q_rad)
             print("n")
             print(n)
 
             #Torque Output Controls
-            u = (B_q @ y) + n
+            #u = (B_q @ y) + n
+            u = self.calc_gravity_compensation_torque(q_rad)
             # --------------------------------------------------------------------------
 
 
