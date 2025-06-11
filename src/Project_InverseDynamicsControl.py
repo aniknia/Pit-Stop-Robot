e22180ce52e166936ed611ab476d908f29be50c1
dylanmryan-ucla <dylanmryan@g.ucla.edu>
2025-06-10 19:24:20 -0700

    opened home position tolerance to 2deg

--- src/Project_InverseDynamicsControl.py
+++ src/Project_InverseDynamicsControl.py
@@ -303,7 +303,7 @@ class InverseDynamicsController:
         
         self.motor_group.angle_rad = home_positions_rad
         time.sleep(0.5)
-        abs_tol = math.radians(1.0)
+        abs_tol = math.radians(2.0)
 
         should_continue_loop = True
         while should_continue_loop:
@@ -311,7 +311,7 @@ class InverseDynamicsController:
             q_rad = self.motor_group.angle_rad
             for dxl_id in home_positions_rad:
                 if abs(home_positions_rad[dxl_id] - q_rad[dxl_id]) > abs_tol:
-                    print(home_positions_rad[dxl_id] - q_rad[dxl_id])
+                    #print(home_positions_rad[dxl_id] - q_rad[dxl_id])
                     should_continue_loop = True
                     break
         
