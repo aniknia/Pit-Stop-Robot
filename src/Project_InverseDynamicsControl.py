788da302844050daad5ea59d7d10591de8d22393
dylanmryan-ucla <dylanmryan@g.ucla.edu>
2025-06-10 21:11:56 -0700

    center of mass adjustments

--- src/Project_InverseDynamicsControl.py
+++ src/Project_InverseDynamicsControl.py
@@ -22,7 +22,7 @@ from mechae263C_helpers.minilabs import FixedFrequencyLoopManager, DCMotorModel
 # Global Variables
 # ------------------------------------------------------------------------------
 # Linkage Lengths
-l1, l2, l3 = 0.15, 0.1, .065
+l1, l2, l3 = 0.15, 0.1, .07
 # ------------------------------------------------------------------------------
 
 class InverseDynamicsController:
@@ -76,7 +76,7 @@ class InverseDynamicsController:
         # Linkage Lengths
         self.l1, self.l2, self.l3 = l1, l2, l3
         # Center of Mass Lengths
-        self.lc1, self.lc2, self.lc3 = 0.12, 0.09, 0.025
+        self.lc1, self.lc2, self.lc3 = 0.13, 0.087, 0.05
         # Width of Link
         self.w1, self.w2, self.w3 = 0.5*(0.036+0.022), 0.5*(0.036+0.022), 0.5*(.036+0.023) 
         # Masses [Kg]
