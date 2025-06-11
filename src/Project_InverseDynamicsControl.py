5e75fc0ccad8ca1d85ec9ddf96fea41ea2c90bc2
dylanmryan-ucla <dylanmryan@g.ucla.edu>
2025-06-10 22:02:02 -0700

    removed inertia matrix

--- src/Project_InverseDynamicsControl.py
+++ src/Project_InverseDynamicsControl.py
@@ -181,7 +181,7 @@ class InverseDynamicsController:
 
             #Torque Output Controls
             #u = (B_q @ y) + n
-            u = (B_q @ y) - self.calc_gravity_compensation_torque(q_rad)
+            u = y - self.calc_gravity_compensation_torque(q_rad)
             # --------------------------------------------------------------------------
 
 
